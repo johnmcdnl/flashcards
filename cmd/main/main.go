327df6bc0f88b94c082509307de0cde8eb6a0986
John McDonnell <john.mcdonnell439@gmail.com>
2018-04-03 19:42:53 +0100

    adding gitignore

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 
+	"github.com/johnmcdnl/flashcards"
+)
+
+func main() {
+	deck := flashcards.NewDeck()
+
+	toJson(deck)
+}
+
+func toJson(i interface{}) {
+	j, _ := json.Marshal(i)
+	fmt.Println(string(j))
+	ioutil.WriteFile("data.json", j, os.ModePerm)
 }
