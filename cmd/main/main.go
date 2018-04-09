0d52632e7915323f4bac1364d91b8f870ea8c9e1
John McDonnell <john.mcdonnell439@gmail.com>
2018-04-10 00:06:48 +0100

    cleanup

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,22 +2,12 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/johnmcdnl/flashcards"
 )
 
-func main() {
-
-	var deck = flashcards.NewStandardDeck()
-	// var deck = flashcards.NewDeckWithSize(flashcards.DeckName, 100, 125, false)
-
-	deck.Know = flashcards.English
-	deck.Learning = flashcards.Russian
-
-	for i := 1; i <= 3; i++ {
-		deck.Next()
-		fmt.Println(deck.Current.PrintQuestion(deck))
-		deck.Current.AttemptAnswer(deck.Know, deck.Learning, "")
-		deck.SaveState()
-	}
+func main(){
+	en := flashcards.English
+	  ru := flashcards.Russian
+	fmt.Println(en)
+	fmt.Println(ru)
 }
--- language.go
+++ language.go
@@ -1,11 +1,20 @@
 package flashcards
 
-// Language is a as it says on the tin :)
-type Language string
 
-// Languages supported
-const (
-	English = Language("English")
-	Russian = Language("Russian")
-	French  = Language("French")
+// Defined Languages
+var (
+	English = language{name: "English", native: "english", isoCode:"en"}
+	Eng = English
+	 Russian = language{name: "Russian", native: "русский", isoCode:"ru"}
+	 Rus = Russian
+	 French = language{name: "French", native: "français", isoCode:"fr"}
+	 Fra = French
 )
+
+//Language represents a ISO 639 Language
+type language struct{
+	name string 
+	native string 
+	isoCode string 
+}
+
