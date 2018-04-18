22b4c12b3799e66237266d1785ca181a19a70a4c
John McDonnell <john.mcdonnell439@gmail.com>
2018-04-18 22:12:23 +0100

    adding frontend

--- data.go
+++ data.go
@@ -1,2 +1,31 @@
 package flashcards
 
+import (
+	"github.com/coreos/bbolt"
+	"os"
+)
+
+type data struct {
+	db     *bolt.DB
+	dbName string
+}
+
+func newData() *data {
+	return &data{
+		db:     new(bolt.DB),
+		dbName: "flashcards.db",
+	}
+}
+
+func (d *data) setValue(bucket, key, value string) {
+	d.db, _  = bolt.Open(d.dbName, os.ModePerm, nil)
+	defer d.db.Close()
+
+	d.db.Update(func(tx *bolt.Tx)error{
+		b := tx.Bucket([]byte(bucket))
+		if b == nil{
+			b, _ = tx.CreateBucketIfNotExists([]byte(bucket))
+		}
+		return b.Put([]byte(key), []byte(value))
+	})
+}
---
+++ temp/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/coreos/bbolt"
+	"os"
+	"github.com/sirupsen/logrus"
+	"fmt"
+)
+
+func main() {
+	data := newData()
+	bucket := "myBucket"
+	data.create(bucket, "myKey", "myValue")
+	data.readAll(bucket)
+}
+
+type data struct {
+	db *bolt.DB
+}
+func newData() *data {
+	d := new(data)
+	d.db = new(bolt.DB)
+	return d
+}
+func (d *data) open() {
+	//d.close()
+	db, _ := bolt.Open("flashcards.db", os.ModePerm, nil)
+	d.db = db
+}
+func (d *data) close() {
+	d.db.Close()
+}
+func (d *data) create(bucket, key, value string) error {
+	return d.update(bucket, key, value)
+}
+func (d *data) read() {
+	defer d.close()
+	d.open()
+
+}
+func (d *data) readAll(bucket string) {
+	defer d.close()
+	d.open()
+
+	tx := func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+		}
+
+		return nil
+	}
+
+	d.db.View(tx)
+
+}
+func (d *data) update(bucket, key, value string) error {
+	defer d.close()
+	d.open()
+
+	tx := func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		if b == nil {
+			logrus.Debugln("(d *data) update() CreateBucketIfNotExists %s", bucket)
+			b, _ = tx.CreateBucketIfNotExists([]byte(bucket))
+		}
+
+		return b.Put([]byte(key), []byte(value))
+	}
+
+	return d.db.Update(tx)
+}
+func (d *data) delete() {}
